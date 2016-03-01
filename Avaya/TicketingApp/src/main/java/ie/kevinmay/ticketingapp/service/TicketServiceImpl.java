f44d769fbf5a0f9e96111fd97882085ee44934d9
Kevin <Kevin@kmay-pc>
2016-03-01 14:37:18 +0000

    Cleaned up errors and warnings

--- Avaya/TicketingApp/src/main/java/ie/kevinmay/ticketingapp/service/TicketServiceImpl.java
+++ Avaya/TicketingApp/src/main/java/ie/kevinmay/ticketingapp/service/TicketServiceImpl.java
@@ -1,9 +1,7 @@
 package ie.kevinmay.ticketingapp.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
