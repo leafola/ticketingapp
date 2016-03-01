c96901fd9c506d94fbe44fb6550348f5bf81c5a0
Kevin <Kevin@kmay-pc>
2016-03-01 17:22:59 +0000

    Attempting to add Swagger

--- Avaya/TicketingApp/src/main/java/ie/kevinmay/ticketingapp/controller/TicketController.java
+++ Avaya/TicketingApp/src/main/java/ie/kevinmay/ticketingapp/controller/TicketController.java
@@ -1,20 +1,24 @@
 package ie.kevinmay.ticketingapp.controller;
 
-import java.util.LinkedHashMap;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
+
+import ie.kevinmay.ticketingapp.model.Ticket;
 
 @Controller
 public class TicketController {
 
-	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@SuppressWarnings("unchecked")
+	@RequestMapping(value = "/list_tickets", method = RequestMethod.GET)
 	public ModelAndView listTickets() {
 		RestTemplate restTemplate = new RestTemplate();
 		String url = "http://localhost:8080/rest/ticketservice/getall";
-		List<LinkedHashMap> tickets = restTemplate.getForObject(url, List.class);
+		List<Ticket> tickets = restTemplate.getForObject(url, List.class);
 		return new ModelAndView("listTickets", "tickets", tickets);
 	}
 }
