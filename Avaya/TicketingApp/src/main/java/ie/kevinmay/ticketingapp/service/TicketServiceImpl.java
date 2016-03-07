0e56df0b25d52512c2a3b7965d806121813c0d87
Kevin <Kevin@kmay-pc>
2016-03-07 12:12:19 +0000

    Commiting before connecting to new SQL tables

--- Avaya/TicketingApp/src/main/java/ie/kevinmay/ticketingapp/service/TicketServiceImpl.java
+++ Avaya/TicketingApp/src/main/java/ie/kevinmay/ticketingapp/service/TicketServiceImpl.java
@@ -12,17 +12,26 @@ import org.springframework.stereotype.Component;
 
 import ie.kevinmay.ticketingapp.dao.TicketDAO;
 import ie.kevinmay.ticketingapp.model.Ticket;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
-@Component
-@Path("/ticketservice")
+@Component // declares TicketService as a Spring bean.
+@Path("/ticketservice") //a JAX-RS annotation that declares TicketService as a "root" JAX-RS resource.
+@Api( value = "/ticketservice", description = "Manage tickets" )
 public class TicketServiceImpl implements TicketService {
 
-	@Autowired
+	@Autowired // requests a reference to the TicketDAO, which Spring will provide. 
 	private TicketDAO ticketDAO;
 
 	@GET
 	@Path("/getall")
 	@Produces({MediaType.APPLICATION_JSON})
+	@ApiOperation( 
+		    value = "List all people", 
+		    notes = "List all people using paging" 
+		   // response = User.class, 
+		   // responseContainer = "List"
+		)
 	public List<Ticket> getAllTickets() {
 		List<Ticket> listTicket = ticketDAO.list();
 
